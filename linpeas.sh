2eae21448809501b046c808b0c39d61e37f3126b
Skajpik <103284636+SkajpCZ@users.noreply.github.com>
2023-03-27 16:34:04 +0200

    Add files via upload

---
+++ linpeas.sh
@@ -0,0 +1,8 @@
+# {"product":"home","version":"16.0.26","path":"Settings.ExclusionsManagement.DetectionExclusions","columns":["Id","Path","ThreatName","Description","FileHash"]}
+4e75e7a1-7250-4400-b69a-bea900000004,D:\Programming\*,"Viry atd.",,
+066db252-ff6f-4ee2-8226-c1bb00000006,D:\Programs\*,Programy,,
+7ee3791c-5a1d-4f0d-8dc6-7a9b00000016,D:\Programming\Skajps\*,,"KOKOT TOTO NE",
+a2f0e01d-8c8c-48bd-9982-abd700000017,"D:\GTA hacky\menus\YimMenu\*",Xenos,,
+9d500a64-26bc-4c04-afc5-c47800000018,D:\Programming\Releases\*,qWS,,
+32e42128-7447-4ed6-9c36-04ca0000001a,C:\Users\Skajp\AppData\Local\Packages\KaliLinux.54290C8133FEE_ey8k8hqnwqnmg,kali,,
+a242950f-dd1c-4acd-9d91-127c0000001b,D:\.Ethical\*,majne,,
